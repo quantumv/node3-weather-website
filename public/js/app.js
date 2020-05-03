708410e07a09eb3e9050fa202c823a8fe5921208
quantum.v.rl <quantum.v.rl@gmail.com>
2020-05-02 23:41:24 -0700

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log('Client side javasript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From JavaScript'
+
+// WIRE-UP
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const location = search.value
+
+    messageOne.textContent = 'Loading Weather'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.err) {
+                messageOne.textContent = data.err
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,106 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+// Step #1
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+
+// Define paths foe Express config
+const publicDirectoryPath = (path.join(__dirname, '../public'))
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to server
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (rep, res) => {
+    res.render('index', {
+        title: 'Weather Globe.com',
+        name: 'Weather Home',
+        author: 'Quantum Vik'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Weather Globe.com',
+        name: 'Quantum Vik',
+        author: 'Quantum Vik'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        text: 'Help me find good weather',
+        title: 'Weather Globe.com',
+        name: 'Help Me!',
+        author: 'Quantum Vik'
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            err: 'You must provide a search term'
+        })
+    }
+    
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/weather', (req,res)=> {
+    if (!req.query.address) {
+        return res.send({
+            err: 'You must provide a search term'
+        })
+    } else {
+        geocode(req.query.address, (err, {latitude, longitude, location} = {}) => {
+            if (err) {
+                return res.send({ err })
+            }
+            forecast(latitude, longitude, (err, forecastData) => {
+                if (err) {
+                    return res.send({ err })
+                }
+                res.send({
+                    forecast: forecastData,
+                    location,
+                    address: req.query.address
+                })
+           })
+        })
+    }
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Weather Globe.com',
+        text: 'Help article found',
+    })        
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Weather Globe.com',
+        text: 'Page not found',
+        author: 'Quantum Vik'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Fired Up!')
+})
