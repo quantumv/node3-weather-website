816298fe5df77abd3df9be4ee4e7a61a647e2820
quantum.v.rl <quantum.v.rl@gmail.com>
2020-05-05 23:02:56 -0700

    Add cloudcover and humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,16 +2,14 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=26ff7bcaa41c940c16d8959e6a1acf69&query=' + latitude + ',' + longitude + '&units=f'
-// Before // request({ url: url, json: true }, (err, response) => {
+    
     request({ url, json: true }, (err, {body}) => {
         if (err) {
             callback('Unable to connect to weather services!', undefined)
-// Before // } else if (response.body.err) {
         } else if (body.err) {
             callback('Unable to connect to location, try another search.', undefined)
         } else {
-            //  callback(undefined, response.body.current.weather_descriptions[0] + '. It is currenlty ' + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + '. There is a ' + response.body.current.precip + '% chance of rain.')
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currenlty ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + '. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + '. There is a ' + body.current.precip + '% chance of rain. ' + ' Humidity meter read for the day is, ' + body.current.humidity + '. Expect ' + body.current.cloudcover + '% chance of overcast skys.')
         }
     })
 }
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,20 +2,16 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +  encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoicXVhbnR1bTIwMTkiLCJhIjoiY2s4emRjdjRsMWhxYjNtbGR1MThyY256aSJ9.LegH48stCsv4Gy0qfhf1fQ&limit=1'
-// Before // request({ url: url, json: true }, (err, response) => {
+
     request({ url, json: true }, (err, {body}) => {
         if (err) {
             callback('Unable to connect to location services!', undefined)
-// Before  // } else if (response.body.features.length === 0) {
         } else if (body.features.length === 0) {
             callback('Unable to connect to find location, try another search.', undefined)
         } else {
             callback(undefined, {
-// Before       //  location: response.body.features[0].place_name,
                 location: body.features[0].place_name,
-                //  latitude: response.body.features[0].center[1],
                 latitude: body.features[0].center[1],
-                // longitude: response.body.features[0].center[0] 
                 longitude: body.features[0].center[0] 
             })
         }
